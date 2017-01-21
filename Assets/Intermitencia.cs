79eb8208de83a5d20fab2b7b60eb4e83a086175a
Santiago Medina <s4nti4gomedin4@gmail.com>
2017-01-21 14:02:55 -0500

    added explosions

---
+++ Assets/Intermitencia.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intermitencia : MonoBehaviour {
+	public Light luzintermitente;
+	public float duration = 4.0F;
+	public float minValue=1.3f;
+	public float maxValue=4.9f;
+	// Use this for initialization
+	void Start () {
+		luzintermitente = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float phi = Time.time / duration * 2 * Mathf.PI;
+		float amplitude =Mathf.Lerp(minValue,maxValue,Mathf.Cos(phi) * 0.5F + 0.5F);
+
+		luzintermitente.intensity = amplitude;
+	}
+}
---
+++ Assets/RandomExplosions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomExplosions : MonoBehaviour {
+
+	public GameObject explosionVfx;
+
+
+	
+	// Update is called once per frame
+	IEnumerator Start () {
+		while (true) {
+			yield return new WaitForSeconds (2f);
+			Instantiate (explosionVfx, new Vector3(Random.Range(-24, 41), 0, Random.Range(-12, 15)), Quaternion.identity);
+			Instantiate (explosionVfx, new Vector3(Random.Range(-24, 41), 0, Random.Range(-12, 15)), Quaternion.identity);
+			Instantiate (explosionVfx, new Vector3(Random.Range(-24, 41), 0, Random.Range(-12, 15)), Quaternion.identity);
+			Instantiate (explosionVfx, new Vector3(Random.Range(-24, 41), 0, Random.Range(-12, 15)), Quaternion.identity);
+		}
+	}
+}
