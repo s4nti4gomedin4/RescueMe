c2aa0e9a8a694b8c46197ef8d1d0016f63d083e9
Santiago Medina Chaverra <s4nti4gomedin4@gmail.com>
2017-01-21 09:47:46 -0500

    adding camera 3er person

--- Assets/Followplayer.cs
+++ Assets/Followplayer.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Followplayer : MonoBehaviour {
 
-	public Transform player;
-	public Vector3 offset;
+	public GameObject target;
+	public float rotationDamping = 1;
+	public float movementDamping = 3;
+	Vector3 offset;
 
-	void Start () {
-		offset = transform.position - player.transform.position;
+	void Start() {
+		offset = target.transform.InverseTransformPoint(transform.position);
 	}
-	
-	void LateUpdate () 
-	{
-		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-		transform.position = player.transform.position + offset;
+
+	void LateUpdate() {
+		float currentAngle = transform.eulerAngles.y;
+		float desiredAngle = target.transform.eulerAngles.y;
+		Quaternion rot = transform.rotation;
+		rot.eulerAngles = new Vector3(0, Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * rotationDamping), 0);
+		transform.rotation = rot;
+		Vector3 desiredPosition = target.transform.TransformPoint(offset);
+		transform.position = Vector3.Lerp (transform.position, desiredPosition, Time.deltaTime * movementDamping); 
+
+		//Quaternion rotation = Quaternion.Euler(0, angle, 0);
+		//transform.position = target.transform.position - (rotation * offset);
+		//transform.LookAt(target.transform);
 	}
 }
--- Assets/_rescueme/scripts/PlayerController.cs
+++ Assets/_rescueme/scripts/PlayerController.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	Rigidbody rb;
+
 	AbilityManager abilityManager;
-	public float movingSpeed;
+
+
+	public float playerSpeed;
+
 	public float speed ;
 	public float maxSpeed;
 	private Vector3 prevPosition;
+
+	//Movement2
+	public float movementSpeed = 10;
+	public float turningSpeed = 60;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -19,15 +28,34 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//Movement1 ();
+		Movement2();
+
+
+
+
+
+		loadSpeedMovement ();
+	}
+	public void Movement2(){
+		float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+		transform.Rotate(0, horizontal, 0);
+
+		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		transform.Translate(0, 0, vertical);
+	}
+	public void loadSpeedMovement(){
+		playerSpeed=Vector3.Distance(prevPosition,transform.position);
+		prevPosition = transform.position;
+	}
+
+	public void Movement1(){
 		float moveX = -Input.GetAxis ("Vertical");
 		float moveZ = Input.GetAxis ("Horizontal");
 		Vector3 forcePosition = new Vector3(moveX,0,moveZ)*speed; 
 		Vector3 force = forcePosition + transform.position;
-		rb.MovePosition (force);
-		transform.LookAt (force);
 
-		movingSpeed=Vector3.Distance(prevPosition,transform.position);
-		prevPosition = transform.position;
+
 
 
 
@@ -44,5 +72,11 @@ public class PlayerController : MonoBehaviour {
 				abilityManager.NextAbility ();
 			}
 		}
+
+		transform.LookAt (force);
+		Vector3 newPosition = transform.position;
+		newPosition += transform.forward *Mathf.Abs(moveX+moveZ);
+		rb.MovePosition (newPosition);
+
 	}
 }
