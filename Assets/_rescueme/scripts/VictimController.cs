d0bd31624814e3b15ccbf40b8a1d58a35634c9bc
Juan Broz <f870421@gmail.com>
2017-01-20 22:49:14 -0500

    changed the victim movement

--- Assets/_rescueme/scripts/VictimController.cs
+++ Assets/_rescueme/scripts/VictimController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class VictimController : MonoBehaviour {
 
 	BoxCollider myCollider;
-	public float speed = 4f;
+	public float speed = 0.1f;
 	public float searchRadius = 2f;
+	Rigidbody rb;
 
 	GameObject rescuer;
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +21,10 @@ public class VictimController : MonoBehaviour {
 			gameObject.transform.LookAt (rescuer.transform);
 			float distance = Vector3.Distance (rescuer.transform.position, gameObject.transform.position);
 			if (distance > searchRadius) {
-				gameObject.transform.position += transform.forward * Time.deltaTime * speed;
+				Vector3 newPosition = transform.forward * speed * Time.deltaTime;
+				Vector3 force = newPosition + transform.position;
+				rb.MovePosition (force);
+
 			}
 		}
 	}
