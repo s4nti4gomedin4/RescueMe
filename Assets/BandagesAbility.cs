a606541a188c42903f4adb2bc360d8e278018621
Juan Broz <f870421@gmail.com>
2017-01-21 11:18:58 -0500

    Firefighter can use a bandage to heal a victim

---
+++ Assets/BandagesAbility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandagesAbility : Ability {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void UseAbility(){
+		print ("Firefighter used a bandage");
+	}
+
+}
