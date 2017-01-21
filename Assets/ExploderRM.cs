2ceb3cc13a6371366731af0d2503a7be2f88243a
Santiago Medina <s4nti4gomedin4@gmail.com>
2017-01-21 11:58:44 -0500

    new file:   Assets/True Explosions/Scenes/bigObject2.unity

---
+++ Assets/ExploderRM.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ExploderRM : Exploder {
+
+	public virtual IEnumerator explode() {
+		ExploderComponent[] components = GetComponents<ExploderComponent> (); 
+
+		foreach (ExploderComponent component in components) {
+			if (component.enabled) {
+				component.onExplosionStarted (this);
+			}
+		}		
+		while (explodeDuration > Time.time - explosionTime) {
+			disableCollider ();
+			for (int i = 0; i < probeCount; i++) {
+				shootFromCurrentPosition ();
+			}
+			enableCollider ();
+			yield return new WaitForFixedUpdate ();
+		}
+	}
+
+	void FixedUpdate() {
+		if (Time.time > explosionTime && !exploded) {
+			exploded = true;
+			StartCoroutine("explode");
+		}
+	}
+}
