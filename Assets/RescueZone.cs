7f574a79cc7d32b72b6a65e34b306fa81381f220
Santiago Medina Chaverra <s4nti4gomedin4@gmail.com>
2017-01-20 22:12:01 -0500

    added map scene

---
+++ Assets/RescueZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescueZone : MonoBehaviour {
+
+	public delegate void RescueZoneEvents (GameObject victim);
+	public static event RescueZoneEvents victimRescue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Victim")) {
+			if (victimRescue != null) {
+				victimRescue (other.gameObject);
+			}
+		}
+	}
+}
